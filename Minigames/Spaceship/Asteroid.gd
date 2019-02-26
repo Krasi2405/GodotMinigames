6429c28c1cde71d7757af202f9d7f2cce556d7dd
Krasimir Georgiev <krasi2405@gmail.com>
2019-02-26 23:39:21 +0200

    Change way boost spawner works by giving over functionality of expiring to boost. Asteroid power up.

---
+++ Minigames/Spaceship/Asteroid.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+export var speed = 10000
+
+var direction = Vector2(0, 0)
+
+func _enter_tree():
+	$CollisionShape2D.disabled = true
+
+
+func _ready():
+	$ActivateCollisionTimer.start()
+	print("asteroid position", position)
+
+
+func _process(delta):
+	var collision = move_and_collide(direction * speed * delta)
+	if(collision != null):
+		var object = collision.collider
+		print("asteroid collided with ", object.name)
+		if(object.has_method("destroy")):
+			object.destroy()
+		queue_free()
+
+func _on_ActivateCollisionTimer_timeout():
+	$CollisionShape2D.disabled = false
+	
+
+func set_direction(direction):
+	self.direction = direction
