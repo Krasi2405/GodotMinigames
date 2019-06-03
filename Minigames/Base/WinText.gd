c7aac04c5304de50946fab4624bbf2a707f16f46
versobaby <vesko.i.georgiev@gmail.com>
2019-06-03 15:33:04 +0300

    Trophies

--- Minigames/Base/WinText.gd
+++ Minigames/Base/WinText.gd
@@ -2,13 +2,28 @@
 Placeholder for showing the winners.
 """
 
-extends HBoxContainer
+extends Control
 
 class_name WinText
 
+var FIRST_PLACE_PNG = preload("res://trophy1.png")
+var SECOND_PLACE_PNG = preload("res://trophy2.png")
+var THIRD_PLACE_PNG = preload("res://trophy3.png")
+var FORTH_PLACE_PNG = preload("res://trophy4.png")
+
+var thropies = [FIRST_PLACE_PNG,SECOND_PLACE_PNG,THIRD_PLACE_PNG,FORTH_PLACE_PNG]
+
+func _ready():
+	parse_winners([1, 4, 2, 3])
+
+
+
 func parse_winners(player_id_win_order : Array):
-	($WinText as RichTextLabel).add_text("Player %d won!\n" % player_id_win_order[0])
-	for i in range(1, player_id_win_order.size()):
-		($WinText as RichTextLabel).add_text(
-				"Player %d placed at %d place\n" % [player_id_win_order[i], i + 1]
-			)
+	for index in player_id_win_order.size():
+		var thropy = thropies[index]
+		var player_id = player_id_win_order[index]
+		
+		(get_node("TextureRect" + str(player_id)) as TextureRect).texture = thropy
+		
+		
+	
